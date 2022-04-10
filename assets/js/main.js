3d297590027cbe95641297b8109894793c7aa036
Ayyappa <Ayyappa@DESKTOP-BAHPIPL>
2022-04-10 17:45:32 +0530

    feat: employee management system application

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+ var x=200;
+ var y=500;
+ var t1="Welcome";
+ var t2="to HTML"
+ var bln=true;
+
+ add();
+
+ function add{
+
+	 var a='10';
+	 var b=a=20;
+
+	 document.writeln(a+b);
+
+	 document.writeln(t1+t2);
+
+	 document.writeln(x+y);
+
+	 document.writeln(bln);
+ }
